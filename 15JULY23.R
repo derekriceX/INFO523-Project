28b12e95493067dbd2642cfc53004a644627b423
Derek Rice <derekrice@arizona.edu>
2023-07-15 08:54:52 -0700

    adding two files - code and an empty text doc - project file itself does not seem to be updating after saving the r file?

---
+++ 15JULY23.R
@@ -0,0 +1,14 @@
+# first comment on project - 2nd commit
+print("hello")
+print("might be getting the handle of git!")
+print("nothing to commit?")
+
+# some homework questions 
+
+print("homework")
+i=1
+while (i<3){
+  
+  print(i)
+  i=i+1
+}
